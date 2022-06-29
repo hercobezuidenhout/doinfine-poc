33873e807ae7d18bae92094cf01def578bf8ae33
Herco Bezuidenhout <herco.bezuidenhout@angloamerican.com>
2022-06-29 09:15:54 +0200

    Create App.js file

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+import Button from "./components/atoms/button/Button";
+
+const App = () => (
+    <div>
+        <h1>Hello World!!</h1>
+        <p>This is a paragraph</p>
+        <Button />
+        <button>I am also here</button>
+    </div>
+)
+
+export default App;

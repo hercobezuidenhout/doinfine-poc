884eabb6e2d1ee8ac923852d289a46a745673b39
Herco Bezuidenhout <herco.bezuidenhout@angloamerican.com>
2022-06-29 09:09:49 +0200

    Update App.js

--- web/src/app.js
+++ web/src/app.js
@@ -5,7 +5,7 @@ import Button from "./components/atoms/button/Button";
 
 const App = () => (
     <div>
-        <h1>Hello World!!</h1>
+        <h1>Hello World!</h1>
         <p>This is a paragraph</p>
         <Button />
         <button>I am also here</button>

60ca08924456b8f3b631e4da064aa74ee064fad3
Herco Bezuidenhout <hercobez@icloud.com>
2022-07-03 12:09:34 +0200

    Remove sum test and update naming convention

--- web/src/__tests__/base.js
+++ web/src/__tests__/base.js
@@ -18,7 +18,9 @@ export * from '@testing-library/react'
 
 export { customRender as render }
 
-test('Base works :)', () => {
-    customRender(<h1 data-testid="hello-world">Hello world</h1>);
-    expect(screen.getByTestId('hello-world')).not.toBeNull();
+describe('@tests/base)', () => {
+    it('runs smoothly', () => {
+        customRender(<h1 data-testid="hello-world">Hello world</h1>);
+        expect(screen.getByTestId('hello-world')).not.toBeNull();
+    })
 })
--- web/src/components/molecules/BottomNavigationBar/BottomNavigationBar.test.js
+++ web/src/components/molecules/BottomNavigationBar/BottomNavigationBar.test.js
@@ -3,8 +3,10 @@ import { render, screen } from '@tests/base';
 import '@testing-library/jest-dom';
 import { BottomNavigationBar } from './BottomNavigationBar';
 
-test('loads and displays BottomNavigationBar', () => {
-    render(<BottomNavigationBar />)
-
-    expect(screen.getByTestId('team-item')).toBeInTheDocument()
+describe('BottomNavigationBar', () => {
+    it('loads and displays BottomNavigationBar', () => {
+        render(<BottomNavigationBar />)
+    
+        expect(screen.getByTestId('team-item')).toBeInTheDocument()
+    })
 })

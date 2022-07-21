7372b83cd3ffea6ce13c768ecb76d1364d35150d
Herco Bezuidenhout <hercobez@icloud.com>
2022-07-21 21:49:50 +0200

    Update TeamFilterBar and BottomNavigationBar

--- web/src/components/organisms/BottomNavigationBar/BottomNavigationBar.js
+++ web/src/components/organisms/BottomNavigationBar/BottomNavigationBar.js
@@ -4,7 +4,10 @@ import { AppBar, Toolbar } from "@mui/material";
 import React, { useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 
-const team = [{ id: 1, name: 'Billy Anderson', avatar: '/img' }]
+const team = [
+    { id: 1, name: 'Billy Anderson', avatar: '/img' },
+    { id: 2, name: 'Steve Wonderland', avatar: '/img' }
+]
 
 export const BottomNavigationBar = () => {
     const location = useLocation();
@@ -16,7 +19,7 @@ export const BottomNavigationBar = () => {
     }, [location])
 
     const renderToolbar = () => {
-        if (pathname === '/team' || pathname === '/') return <TeamFilterBar team={team} />
+        if (pathname === '/team' || pathname === '/') return <TeamFilterBar team={team} activeMember={team[1]} />
         if (pathname === '/company') return <CompanyFilterBar />
     }
 

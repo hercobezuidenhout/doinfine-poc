74e96a6ffdf1f9254caebe298324cb36c0d9a598
Herco Bezuidenhout <hercobez@icloud.com>
2022-08-03 12:02:15 +0200

    Update team path to require an id

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { BottomNavigationBar } from '@components/organisms';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { MenuBar } from '@components/molecules';
 import { Box, Container, CssBaseline } from '@mui/material';
 
 export const App = () => {
     const [title, setTitle] = useState()
     const location = useLocation()
+    const navigate = useNavigate()
 
     useEffect(() => {
         const pathname = location.pathname
-        if (pathname === '/' || pathname === '/team') setTitle('Team')
+
+        if (pathname === '/') navigate('/team/1')
+        if (pathname === '/team') setTitle('Team')
         if (pathname === '/company') setTitle('Leaderboard')
     }, [location])
 
--- web/src/providers/RouterProvider.js
+++ web/src/providers/RouterProvider.js
@@ -4,12 +4,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { App } from '../App';
 
 export const RouterProvider = () => {
-
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />}>
-                    <Route index path="team" element={<TeamPage />} />
+                    <Route path="team/:id" element={<TeamPage />} />
                     <Route path="company" element={<div>Company Page</div>} />
                 </Route>
             </Routes>

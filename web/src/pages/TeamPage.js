9fef5817d7b77ce4f6e2d762717dff902c899869
Herco Bezuidenhout <herco.bezuidenhout@angloamerican.com>
2022-08-04 10:00:53 +0200

    Start with team page

--- web/src/pages/TeamPage.js
+++ web/src/pages/TeamPage.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useTeamContext } from '@providers/TeamProvider'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 export const TeamPage = () => {
+    const team = useTeamContext()
+    const [searchParams] = useSearchParams()
+    const [member, setMember] = useState()
+
+    useEffect(() => {
+        if (!team) return
+        const teamMember = team.members.filter(x => x.id == searchParams.get('member'))[0]
+        setMember(teamMember)
+    }, [searchParams])
+
     return (
         <div>
-            <h1 data-testid="team-page-title">Team Member</h1>
+            <h1 data-testid="team-page-title">{member && `${member.firstName} ${member.lastName}`}</h1>
         </div>
     )
 }

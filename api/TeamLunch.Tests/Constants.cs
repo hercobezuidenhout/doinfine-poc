caf4645ec2d7ca2ae986883607c6fd06275fdd25
Herco Bezuidenhout <hercobez@icloud.com>
2022-09-20 20:06:56 +0200

    Create GetUserById query

--- api/TeamLunch.Tests/Constants.cs
+++ api/TeamLunch.Tests/Constants.cs
@@ -3,4 +3,6 @@ namespace TeamLunch.Tests;
 public static class Constants
 {
     public static string EXAMPLE_USER_ID = "3e64b61f-ce47-4f30-8d4e-4de4869a07ad";
+    public static string EXAMPLE_USERNAME = "billy";
+    public static string EXAMPLE_FULLNAME = "Billy Anderson";
 }
---
+++ api/TeamLunch/Queries/GetUserById.cs
@@ -0,0 +1,40 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TeamLunch.Data;
+using TeamLunch.Exceptions;
+
+namespace TeamLunch.Queries;
+
+public static class GetUserById
+{
+    public record Query(string id) : IRequest<Response>;
+
+    public class Handler : IRequestHandler<Query, Response>
+    {
+        private readonly DataContext _db;
+
+        public Handler(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var user = _db.Users.Where(x => x.Id == request.id).Include(x => x.Teams).First();
+                return new Response(
+                    user.Id,
+                    user.Username,
+                    string.Concat(user.FirstName, " ", user.LastName),
+                    user.Teams.Select(x => x.Id).ToList()
+                );
+            }
+            catch (InvalidOperationException exception)
+            {
+                throw new UserNotFoundException($"The user with id {request.id} was not found.", exception);
+            }
+        }
+    }
+    public record Response(string id, string username, string fullName, List<int> teams);
+}

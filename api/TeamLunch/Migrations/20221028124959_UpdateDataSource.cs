de78aad2c241ee3eff0d779e401784c78193a00e
Herco Bezuidenhout <hercobez@icloud.com>
2022-10-28 14:50:10 +0200

    Update migrations

---
+++ api/TeamLunch/Migrations/20221028124959_UpdateDataSource.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamLunch.Migrations
+{
+    public partial class UpdateDataSource : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

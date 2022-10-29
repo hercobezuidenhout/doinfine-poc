4da8ef1e3c70084d9c11ecef8e1d18aca18ce033
Herco Bezuidenhout <hercobez@icloud.com>
2022-10-29 11:01:26 +0200

    Apply migrations

--- api/TeamLunch/Migrations/20221029090001_SeedData.Designer.cs
+++ api/TeamLunch/Migrations/20221029090001_SeedData.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TeamLunch.Data;
 
@@ -10,9 +11,10 @@ using TeamLunch.Data;
 namespace TeamLunch.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20221029090001_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.10");

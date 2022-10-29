99e83023ccafc33cf04751e935d07bbfb4bd55fe
Herco Bezuidenhout <hercobez@icloud.com>
2022-10-29 12:43:44 +0200

    Update migrations

--- api/TeamLunch/Migrations/20221029104339_RemoveSeedData.Designer.cs
+++ api/TeamLunch/Migrations/20221029104339_RemoveSeedData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TeamLunch.Data;
 
@@ -11,9 +12,10 @@ using TeamLunch.Data;
 namespace TeamLunch.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20221029104339_RemoveSeedData")]
+    partial class RemoveSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

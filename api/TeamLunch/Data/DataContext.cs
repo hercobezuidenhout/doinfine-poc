b678346edaaab4f4fffc804c7a8cd45cba790a96
Herco Bezuidenhout <hercobez@icloud.com>
2022-10-29 12:07:21 +0200

    Remove SeedData

--- api/TeamLunch/Data/DataContext.cs
+++ api/TeamLunch/Data/DataContext.cs
@@ -38,7 +38,7 @@ public class DataContext : DbContext
             .WithMany(n => n.UserNotifications)
             .HasForeignKey(un => un.NotificationId);
 
-        SeedData(builder);
+        // SeedData(builder);
     }
 
     private void SeedData(ModelBuilder builder)

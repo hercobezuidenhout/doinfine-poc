aec604d1b4f31244f886c65568f413a81804d32a
Herco Bezuidenhout <hercobez@icloud.com>
2022-12-05 13:44:24 +0200

    Update Seed data for test environment

--- api/TeamLunch/Data/DataContext.cs
+++ api/TeamLunch/Data/DataContext.cs
@@ -68,11 +68,14 @@ public class DataContext : DbContext
         );
 
         builder.Entity<FineRequest>().HasData(
-            new FineRequest { Id = 1, TeamId = 1, Finer = "Lcjh1S4gKJaPbOjWB56BUs9hS5u2", Finee = "rHB5wC8hajM1PvyoMplClDlworj1", Reason = "not caring enough about water" },
+            new FineRequest { Id = 1, TeamId = 1, Finer = "LtdmLyKS29dB3uNfI7qMaTYQ5pa2", Finee = "rHB5wC8hajM1PvyoMplClDlworj1", Reason = "not caring enough about water" },
             new FineRequest { Id = 2, TeamId = 1, Finer = "rHB5wC8hajM1PvyoMplClDlworj1", Finee = "Lcjh1S4gKJaPbOjWB56BUs9hS5u2", Reason = "not caring enough about dogs" },
             new FineRequest { Id = 3, TeamId = 1, Finer = "Lcjh1S4gKJaPbOjWB56BUs9hS5u2", Finee = "rHB5wC8hajM1PvyoMplClDlworj1", Reason = "not caring enough about something else" },
             new FineRequest { Id = 4, TeamId = 1, Finer = "Lcjh1S4gKJaPbOjWB56BUs9hS5u2", Finee = "rHB5wC8hajM1PvyoMplClDlworj1", Reason = "not caring enough about something else" },
             new FineRequest { Id = 5, TeamId = 1, Finer = "Lcjh1S4gKJaPbOjWB56BUs9hS5u2", Finee = "rHB5wC8hajM1PvyoMplClDlworj1", Reason = "not caring enough about something else" }
+        );
+        builder.Entity<PaymentRequest>().HasData(
+            new PaymentRequest { Id = 1, TeamId = 1, UserId = "LtdmLyKS29dB3uNfI7qMaTYQ5pa2", Action = "telling a dad-joke during standup" }
         );
 
         builder.Entity<FineRequestResponse>().HasData(
